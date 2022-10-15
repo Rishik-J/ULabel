af7cf9d1ff3c41c44d50363b3f1cee380b1eff4f
rishik <rishikjanaswamy999@gmail.com>
2022-10-15 09:32:24 -0400

    Created App

---
+++ ULabel/Shared/ULabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ULabelApp.swift
+//  Shared
+//
+//  Created by Rishik Dev on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ULabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
